0f6bba0140328d05749b0b6f4c581947cf6dcc62
ibrahim013 <walaibrahim13@gmail.com>
2018-07-20 23:21:08 +0100

    shippment

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 // import { LoginComponent } from './component/LoginComponent';
 import Dashboard from './component/Dashboard';
 import { LandingPage } from './component/LandingPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
+import {ShipmentList} from './component/ShipmentList'
+
 
 export const App = () => (
     <Router>
       <Switch>
         <Route exact path='/' component={LandingPage} />
         <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact='/view-shipment' component={ShipmentList} />
       </Switch>
     </Router>
 );
---
+++ src/component/ShipmentList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ShipmentList = () =>(
+<div>
+    <div className='row'>
+    <p>View All Shipment</p>
+    </div>
+</div>
+)
