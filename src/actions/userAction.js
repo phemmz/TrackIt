aadb8ee756161817b4cf1ecbd856036dbfa55fe2
ibrahim013 <walaibrahim13@gmail.com>
2018-07-21 08:13:06 +0100

    login-page-fix

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,5 +1,5 @@
 import { LOGIN_SUCCESS } from './actionTypes';
-import Axios from '../../node_modules/axios';
+import Axios from 'axios';
 import decodedToken from '../helper/decodeToken'
 
 export const loginUserSuccess = () => {
@@ -13,8 +13,8 @@ return dispatch => Axios.post('https://unilever-track-it.herokuapp.com/api/v1/lo
     if(response==='unauthorize'){
         localStorage.setItem('error', 'unauthorize')   
     }
-    localStorage.setItem('token', response.data.auth_token)    
-    dispatch(decodedToken(response))
-    return true;
+    localStorage.setItem('auth_token', response.data.auth_token)    
+        return dispatch(decodedToken(response))
+        window.location.href ='/dashboard'
 })
 }
