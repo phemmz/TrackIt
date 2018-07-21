93e08e22d6582ee6a6eb7db1ccbc6151c204bcd3
Adetunji Femi <phemmzmcllroy@gmail.com>
2018-07-21 03:52:15 +0100

    Add pdf conversion logic

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,6 +1,75 @@
 import React from 'react';
+const PDFJS = require('pdfjs-dist');
 
 export default class Dashboard extends React.Component {
+
+  callbackAllDone = (fullText) => {
+    console.log(fullText, ' fuuuuuullltext')
+  }
+
+  callbackPageDone = (complete, total) => {
+    console.log(complete, ' complete', total, ' total');
+  }
+
+  pdfToText = (data, callbackPageDone, callbackAllDone) => {
+    let complete = 0;
+
+    console.log( data  instanceof ArrayBuffer  || typeof data == 'string' );
+    PDFJS.getDocument("./BOHYUNGPARK.pdf").then(function(pdf) {
+      var div = document.getElementById('viewer');
+      var total = pdf.numPages;
+      callbackPageDone(0, total);
+      var layers = {};
+      
+      for (i = 1; i <= total; i++) {
+        pdf.getPage(i).then(function(page) {
+          var n = page.pageNumber;
+
+          page.getTextContent().then(function(textContent) {
+            if(null != textContent.bidiTexts) {
+              var page_text = "";
+              var last_block = null;
+              for(var k = 0; k < textContent.bidiTexts.length; k++) {
+                var block = textContent.bidiTexts[k];
+
+                if (last_block != null && last_block.str[last_block.str.length-1] != ' ') {
+                  if (block.x < last_block.x)
+                    page_text += "\r\n"; 
+                  else if (last_block.y != block.y && (last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) == null))
+                    page_text += ' ';
+                }
+
+                page_text += block.str;
+                last_block = block;
+              }
+
+              textContent != null && console.log("page " + n + " finished."); //" content: \n" + page_text);
+              layers[n] =  page_text + "\n\n";
+            }
+            ++complete;
+            callbackPageDone(complete, total);
+
+            if (complete === total) {
+              window.setTimeout(function() {
+                var full_text = "";
+                var num_pages = Object.keys(layers).length;
+
+                for( var j = 1; j <= num_pages; j++) {
+                  full_text += layers[j];
+                }
+                callbackAllDone(full_text);
+              }, 1000);              
+            }
+          }); // end  of page.getTextContent().then
+        }) // end of page.then
+      } // of for
+    });
+  }
+
+  handleFileUpload = (event) => {
+    console.log(event.target.files, ' fffiifle')
+    this.pdfToText(event.target.files, this.callbackPageDone, this.callbackAllDone);
+  }
 
   handleInputChange = () => {
     console.log('hhhh')
@@ -23,7 +92,7 @@ export default class Dashboard extends React.Component {
                   type="file"
                   className="custom-file-input upload__pfi__section"
                   id="pfiFile"
-                  onChange={this.handleInputChange}
+                  onChange={this.handleFileUpload}
                 />
                 <label className="custom-file-label" htmlFor="customFile">Upload PFI</label>
               </div>
